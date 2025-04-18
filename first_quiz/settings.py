051a6b42ce13bc55a45dc15898a559ff91f55ab9
rlatpsud <66771585+rlatpsud@users.noreply.github.com>
2025-04-18 11:22:35 +0900

    Update settings.py

--- first_quiz/settings.py
+++ first_quiz/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '68tvhm!$rwxiv#79b+id(^%5-lo_c&l2g!aibknb)k!!a92#+o')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
